cdb0a4ede6fed120fd2582e05e7c09e8f5012f8e
Md Saquib Hussain <mdsaquib28official@gmail.com>
2025-02-18 22:32:11 +0530

    "Revert Backed"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import AnimatedBackground from "../components/home/AnimatedBackground";
-import HeroContent1 from "../components/home/HeroSection";
+import HeroContent from "../components/home/HeroSection";
 
 const Home = () => {
   return (
     <main className="relative min-h-screen bg-black overflow-hidden">
       <AnimatedBackground />
-      <HeroContent1 />
+      <HeroContent />
     </main>
   );
 };
